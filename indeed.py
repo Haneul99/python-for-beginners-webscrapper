3412c66e77ad054465fa53049beb29d00441513f
Haneul99 <healing5746@gmail.com>
2021-01-19 00:32:57 +0900

    title error fixed

--- indeed.py
+++ indeed.py
@@ -19,9 +19,7 @@ def extract_indeed_pages():
     return max_page
 
 def extract_job(html):
-    if html.find("div", {"class": "title"}) is not None:
-        title = html.find("div", {"class": "title"}).find("a")["title"]
-        print(title)
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
     company_anchor = company.find("a")
     if company_anchor is not None:
@@ -32,7 +30,7 @@ def extract_job(html):
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
     return {
-        #'title': title,
+        'title': title,
         'company': company,
         'location': location,
         'link': f"https://www.indeed.com/viewjob?jk={job_id}"
@@ -41,12 +39,11 @@ def extract_job(html):
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    for page in range(last_page):
+    for page in range(1):
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
             job = extract_job(result)
-            print(job)
             jobs.append(job)
     return jobs
